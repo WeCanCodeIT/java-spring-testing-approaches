4638e4e5100a8d2e5785573b376376e5b9e2e532
B T Forsythe <brian@btforsythe.io>
2018-02-23 14:36:42 -0500

    initial commit

---
+++ src/main/java/org/wecancodeit/spring/testing/approaches/ApproachesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.spring.testing.approaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApproachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApproachesApplication.class, args);
+	}
+}
